a6c305a17d06a5fd512fd4d92d9a27ff0349c448
EmanuelBorror <157421234+EmanuelBorror@users.noreply.github.com>
2025-08-02 20:53:10 -0400

    Add files via upload

--- nrn_sep.py
+++ nrn_sep.py
@@ -1,20 +1,12 @@
-from swcToolkit import swcToolkit
 import os 
-import numpy as np
-import sys
-
 input_file = 'test_retinal_cells.swc' # replace with your SWC file path
 output_path = 'C:\\Users\\emanu\\USC-Summer\\nrn_sep_files'  # replace with your desired output directory
-
-# swc = swcToolkit() # load swc tool kit for use throughout
-# swc_data = swc.read_swc_file(input_file)  # Read SWC data
-# # swc_data = pd.read_csv(input_file, sep=r'\s+', comment='#', header=None) # Read SWC data using pandas
-# print("Size:", np.shape(swc_data))
-# print("Length:", np.size(swc_data))
 
 def main(input_file, output_path): 
     """
     Main function to separate neurons based on SWC data.
+    :param input_file: Path to the input SWC file.
+    :param output_path: Path to save the output files.
     """
     import sys
     from swcToolkit import swcToolkit  # Import the SWC toolkit
@@ -29,8 +21,8 @@ def main(input_file, output_path):
     # structure_identifier = 1 # column # in swc format for structure identifier
     # parent_sample = 6 # column # in swc format for parent node
     # sample_num = 0 # column # in swc format for sample number
-    soma_indices = soma_id_and_orphan(swc_data)
-    dictionary_data = data_into_dictionary(swc_data)
+    soma_indices = soma_id_and_orphan(swc_data) # array of soma indices from swc data
+    dictionary_data = data_into_dictionary(swc_data) # 
     num_soma = 0
     returned_nodes = 0
     sample_col = 0
@@ -86,40 +78,11 @@ def soma_id_and_orphan(data): # Seems to work fine
             orphans.append(index)
     return soma, orphans
 
-# def sep_nrn(data, soma_index):
-#     """
-#     Separate neurons based on soma indices.
-#     :param data: SWC data as a pandas DataFrame.
-#     :param soma_indices: List of soma indices.
-#     :return: List of separated neurons.
-#     """
-#     # structure_identifier = 1 # column # in swc format for structure identifier
-#     parent_sample = 6 # column # in swc format for parent node
-#     # sample_num = 0 # column # in swc format for sample number
-#     nrn = []
-#     parents = []
-#     parents.append(soma_index) # Start with soma indices as parents
-#     nrn.append(data[soma_index])  # Append the soma node to the neuron list
-#     # print("Soma parent found:", parents)
-#     while len(parents) > 0:
-#         # print("Current parents:", parents)
-#         for n in parents: 
-#             new_parents = []
-#             for index in range(len(data)): 
-#                 if data[index][parent_sample] == n + 1: 
-#                     nrn.append(data[index])
-#                     new_parents.append(index)
-#             parents = new_parents # This part overwrites the parents list with the new parents found 
-# # write recursive function to find all children of a parent node
-# # use with dictionary to store the children of each parent node
-# # AI: pytorch (easy) TinserFlow (hard)
-# # data arrays called tinsers
-# # conceptualize numpy arrays as tensors
-
-#     print("Length of neuron data after separation:", len(nrn))
-#     return nrn           
-
 def find_indices(arr, targets):
+    """Finds all indices in an array that have the values listed in the targets array
+    :param arr: An array of data to be searched through
+    :param targets: An array of values to search for
+    :return output: An array with the indices of every value that matches a value described in the target array"""
     target_set = set(targets)
     output = [i + 1 for i, v in enumerate(arr) if v in target_set]
     if len(output) == 0: 
@@ -129,32 +92,40 @@ def find_indices(arr, targets):
 # print(find_indices(p, [12]))
 
 def add_dict_entries(dictionary): 
+    """Returns one array as a combination of many smaller arrays each with individual dictionary keys
+    :param dictionary: The dictionary full of arrays to combine into one array
+    :return output: One array consisting of all array values in the dictionary from every key"""
     output = []
     for i in range(len(dictionary) - 1): 
         output += dictionary[i + 1]
     return output
-# Example Usage 
-# print(add_dict_entries(n))
 
 def soma_tree(dictionary, iteration, data, num_nodes): 
-    # print(iteration)
-    if dictionary.get(iteration) == None: 
+    """Separates individual neurons from network of neurons using swc data and dictionary keys. It finds all nodes connected to the previously located nodes. 
+    :param dictionary: A dictionary with each key as an array of nodes connected to the nodes described in the previous key
+    :param iteration: The number of iterations the recursive function has completed .
+    :param data: The data of parent nodes in dictionary format to be updated each iteration.
+    :param num_nodes: The total number of nodes that have been filtered through and isolated into an individual neuron. 
+    :return output_arr: A single array of all indices in the swc_data that compose a single neuron.
+    :return num_nodes: The total number of nodes that were isolated after isolating one individual neuron."""
+    if dictionary.get(iteration) == None: # Checks and returns all indices of raw swc data of a specific soma starting point
         output_arr = add_dict_entries(dictionary)
         num_nodes += len(output_arr)
         return output_arr, num_nodes
     else: 
-        # print(dictionary)
-        parents = dictionary.get(iteration)
+        parents = dictionary.get(iteration) # Recursive function that finds the indices of nodes that make up individual neurons with soma as starting points
         new_parents = find_indices(data['p'], parents)
-        # print(new_parents)
         dictionary.update({(iteration + 1): new_parents})
         iteration += 1
         return soma_tree(dictionary, iteration, data, num_nodes)
 
 def data_into_dictionary(data): 
-    sample_num = 0 
+    """Reorganizes raw data into a dictionary format for use in other functions.
+    :param data: swc data as a pandas dataFrame.
+    :return output_dictionary: dictionary with sample numbers and parent node information of swc data."""
+    sample_num = 0 # define swc convention constants
     parent_sample = 6 
-    indices_arr = []
+    indices_arr = [] # initialize arrays for use in function. 
     parents_arr = []
     for i in range(len(data)): 
         indices_arr.append(data[i][sample_num])
@@ -163,6 +134,10 @@ def data_into_dictionary(data):
     return(output_dictionary)
     
 def data_idx_to_data(swc_data, idx_arr):
+    """Gathers all raw swc data from pandas dataframe from array that holds index values of wanted data. 
+    :param swc_data: Raw swc_data in pandas datafram format
+    :param idx_array: An array of sample number indices of individual neuron cells.
+    :return: Pandas Dataframe of swc raw data with only the indices of wanted data."""
     filtered_data = []
     # print(idx_arr)
     for index in idx_arr: 
@@ -170,12 +145,12 @@ def data_idx_to_data(swc_data, idx_arr):
     return filtered_data
 
 def redef_idx(data): 
-    """Redefine indices in the SWC data.
+    """Redefine indices in the SWC data to match swc format convensions after node isolation.
     :param data: swc data as a pandas dataFrame.
     :return: DataFrame with redefined indices."""
-    sample_num = 0
+    sample_num = 0 # Set constants for use
     parent_sample = 6
-    new_sample_num = 0 
+    new_sample_num = 0 # Initialize first sample number for swc format
 
     for idx in range(len(data)): 
         new_sample_num += 1
@@ -183,38 +158,29 @@ def redef_idx(data):
             if data[idx][sample_num] == data[smpl][parent_sample]:
                 data[smpl][parent_sample] = new_sample_num
         data[idx][sample_num] = new_sample_num
-    # print("Length of data after redefining indices:", len(data))
     return data  # Return the modified data           
 
-def file(nrn_data, output_path, file_num, input_name): # Works Great
+def file(nrn_data, output_path, file_num, input_name): 
     """
     Write separated neurons to files.
     :param nrn: Separated neuron. 
     :param output_path: Path to save the output files.
     """
     import os
-    # structure_identifier = 1 # column # in swc format for structure identifier
-    # parent_sample = 6 # column # in swc format for parent node
-    # sample_num = 0 # column # in swc format for sample number
-    if not os.path.exists(output_path):
+    if not os.path.exists(output_path): # Create export path
         os.makedirs(output_path)
     
-    file_name = os.path.join(output_path, f'{input_name}_{file_num}.swc')
+    file_name = os.path.join(output_path, f'{input_name}_{file_num}.swc') # creates file name
     # f'
-    with open(file_name, 'w') as f:
+    with open(file_name, 'w') as f: # exports new file to export path
         for node in nrn_data:
             f.write(' '.join(map(str, node)) + '\n')
 
 if __name__ == '__main__':
-    input_file = 'test_retinal_cells.swc'  # replace with your SWC file path
-    output_path = 'C:\\Users\\emanu\\USC-Summer\\nrn_sep_files'  # replace with your desired output directory
+    # input_file = 'test_retinal_cells.swc'  # replace with your SWC file path
+    # output_path = 'C:\\Users\\emanu\\USC-Summer\\nrn_sep_files'  # replace with your desired output directory
     main(input_file, output_path)
 
-# Write another function that reads all the nodes that are stored in a new nrn function, and removes it from the swc_data file
-# This way you can make the process quicker as you read less nodes each time, and you minimize repeated data
 
-# somas = soma_id(swc_data)
-# sep_nrn_data = sep_nrn(swc_data, somas[0])  # Example usage with the first soma index
-# print(sep_nrn_data)
 
 
