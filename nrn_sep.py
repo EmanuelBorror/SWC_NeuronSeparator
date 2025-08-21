e98dc8582ddd74df8bae6b2a263b85fec2cc9343
EmanuelBorror <157421234+EmanuelBorror@users.noreply.github.com>
2025-08-21 13:44:53 -0700

    Add files via upload

--- nrn_sep.py
+++ nrn_sep.py
@@ -12,12 +12,14 @@ def main(input_file, output_path):
     from swcToolkit import swcToolkit  # Import the SWC toolkit
     # import os  # Import os for file operations
     file_name = os.path.splitext(input_file)[0]
-    orphan_file = 'orphan'
     swc = swcToolkit() # load swc tool kit for use throughout
     swc_data = swc.read_swc_file(input_file)  # Read SWC data
     recursion_limit = len(swc_data) + 1 # changes the recursion limit so that recursive function can be called more than 5 times.
     sys.setrecursionlimit(recursion_limit)
-
+    # Find soma indices in the SWC data
+    # structure_identifier = 1 # column # in swc format for structure identifier
+    # parent_sample = 6 # column # in swc format for parent node
+    # sample_num = 0 # column # in swc format for sample number
     soma_indices = soma_id_and_orphan(swc_data) # array of soma indices from swc data
     dictionary_data = data_into_dictionary(swc_data) # 
     num_soma = 0
@@ -30,34 +32,47 @@ def main(input_file, output_path):
     for idx in range(len(soma_indices[0])): 
         num_nodes = 0
         iteration = 1
-        tree = {1: [swc_data[idx][sample_col]]}
+        swc_data_index = soma_indices[0][idx]
+        # print(swc_data_index, 'is the current soma index in the swc_data.\n')
+        tree = {1: [swc_data[swc_data_index][sample_col]]}
         new_tree = soma_tree(tree, iteration, dictionary_data, num_nodes)
-        # temp_data = sep_nrn(swc_data, idx)
-        # print("Number of nodes separated: ", new_tree[1])
         unfiltered_nrn_data = data_idx_to_data(swc_data, new_tree[0])
         returned_nodes += new_tree[1]
         nrn_data = redef_idx(unfiltered_nrn_data)
         num_soma += 1 
-        file(nrn_data, output_path, num_soma, file_name)  # Write se parated neurons to files
+        file(nrn_data, output_path, num_soma, file_name, soma_file = True)  # Write se parated neurons to files
         print(returned_nodes, 'nodes separated.\n')
 
     for idx in range(len(soma_indices[1])): 
         num_nodes = 0
         iteration = 1
-        tree = {1: [swc_data[idx][sample_col]]}
+        swc_data_index = soma_indices[1][idx]
+        # print(swc_data_index, 'is the current orphan start node index in the swc_data.\n')
+        tree = {1: [swc_data[swc_data_index][sample_col]]}
         new_tree = soma_tree(tree, iteration, dictionary_data, num_nodes)
-        # temp_data = sep_nrn(swc_data, idx)
-        # print("Number of nodes separated: ", new_tree[1])
         unfiltered_nrn_data = data_idx_to_data(swc_data, new_tree[0])
         r_orphan_nodes += new_tree[1]
         nrn_data = redef_idx(unfiltered_nrn_data)
         num_orphans += 1 
-        file(nrn_data, output_path, num_orphans, orphan_file)  # Write separated neurons to files
+        file(nrn_data, output_path, num_orphans, file_name, soma_file = False)  # Write separated neurons to files
         print(r_orphan_nodes, 'nodes connected to an orphan node.\n')
-    if (returned_nodes + len(soma_indices[1]) == len(swc_data)) or (r_orphan_nodes + len(soma_indices[1]) == len(swc_data)): 
-        print('All soma originating nodes plus orphan nodes separated.\n')
+    # Check if all nodes were separated
+    if (returned_nodes + r_orphan_nodes == len(swc_data)): 
+        print('All nodes separated successfully.\n')
+        print('Total number of nodes in swc data:', len(swc_data))
+        print('Total number of nodes separated:', returned_nodes + r_orphan_nodes)
+        print('Total number of soma nodes: ', len(soma_indices[0]))
+        print('Total number of orphan nodes: ', len(soma_indices[1]))
+        print('Total number of nodes connected to soma:', returned_nodes)
+        print('Total number of nodes connected to orphan nodes:', r_orphan_nodes)
     else: 
-        print('Not all nodes were separated. \n Please check the SWC data for errors, or orphan node connections.')
+        print('Not all nodes were separated. \nPlease check the SWC data for errors, or orphan node connections.')
+        print('Total number of nodes in swc data:', len(swc_data))
+        print('Total number of nodes separated:', returned_nodes + r_orphan_nodes)
+        print('Total number of soma nodes: ', len(soma_indices[0]))
+        print('Total number of orphan nodes: ', len(soma_indices[1]))
+        print('Total number of nodes connected to soma:', returned_nodes)
+        print('Total number of nodes connected to orphan nodes:', r_orphan_nodes)
         
 def soma_id_and_orphan(data): # Seems to work fine
     """
@@ -147,19 +162,43 @@ def redef_idx(data):
     """Redefine indices in the SWC data to match swc format convensions after node isolation.
     :param data: swc data as a pandas dataFrame.
     :return: DataFrame with redefined indices."""
-    sample_num = 0 # Set constants for use
-    parent_sample = 6
+    from pprint import pprint
+    import numpy as np
     new_sample_num = 0 # Initialize first sample number for swc format
-
-    for idx in range(len(data)): 
+    data = sort_by_id(data)
+    # pprint(data)
+    for i in range(len(data)): 
         new_sample_num += 1
-        for smpl in range(len(data)): 
-            if data[idx][sample_num] == data[smpl][parent_sample]:
-                data[smpl][parent_sample] = new_sample_num
-        data[idx][sample_num] = new_sample_num
-    return data  # Return the modified data           
+        current_id = data[i]['id']
+        # print("Checking Parents equal to:", current_id) 
+        for j in range(len(data)): 
+            current_parent = data[j]['parent']
+            # print(current_parent)
+            if (current_parent == current_id):
+                data[j]['parent'] = new_sample_num
+        data[i]['id'] = new_sample_num
+    return data      
 
-def file(nrn_data, output_path, file_num, input_name): 
+def sort_by_id(data): 
+    """Sorts SWC data by ID value so it starts at the lowest and ends at the highest value to counter overwriting.
+    :param data: swc data as a pandas dataFrame.
+    :return: DataFrame sorted by ID."""
+    id_list = [] 
+    new_data = [] 
+    for i in range(len(data)): 
+        id_list.append(data[i]['id'])
+    id_list.sort()
+    # print(len(id_list))
+    # print(id_list, '\n')
+    for i in range(len(id_list)): 
+        find_value = id_list[i]
+        for j in range(len(data)):
+            if data[j]['id'] == find_value:
+                new_data.append(data[j])
+                break
+    return new_data
+
+def file(nrn_data, output_path, file_num, input_name, soma_file): 
     """
     Write separated neurons to files.
     :param nrn: Separated neuron. 
@@ -168,18 +207,18 @@ def file(nrn_data, output_path, file_num, input_name):
     import os
     if not os.path.exists(output_path): # Create export path
         os.makedirs(output_path)
-    
-    file_name = os.path.join(output_path, f'{input_name}_{file_num}.swc') # creates file name
-    # f'
+    if soma_file == True: # If the file is a soma file, add soma to the file name
+        file_name = os.path.join(output_path, f'{input_name}_{file_num}.swc') # creates file name
+    elif soma_file == False: 
+        file_name = os.path.join(output_path, f'{input_name}_orphan_{file_num}.swc') # creates file name for orphan nodes
     with open(file_name, 'w') as f: # exports new file to export path
         for node in nrn_data:
             f.write(' '.join(map(str, node)) + '\n')
 
 if __name__ == '__main__':
-    # input_file = 'test_retinal_cells.swc'  # replace with your SWC file path
+    # input_file = 'swc_text_test.swc'  # replace with your SWC file path
     # output_path = 'C:\\Users\\emanu\\USC-Summer\\nrn_sep_files'  # replace with your desired output directory
     main(input_file, output_path)
-
 
 
 
